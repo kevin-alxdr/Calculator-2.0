b8ca39dbc2f9ea045f5bbc2032b1af68d66b707c
Kevin Alexander <72028357+kevin-alxdr@users.noreply.github.com>
2023-10-13 15:30:41 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,88 @@
+﻿namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            string again = "y";
+            while (again == "y")
+
+            {
+                Console.WriteLine("\n--------------------------Calculator--------------------------\n");
+                Console.WriteLine("Please enter 2 numbers with a symbol between them: (+ - * /) ");
+                Console.WriteLine(" ");
+                string line = Console.ReadLine();
+
+                line = line.Replace(" ", string.Empty);
+
+
+                // ADD
+                // --------------------------------------------------------------
+
+                if (line.Contains("+"))
+                {
+
+                    string[] subs = line.Split('+');
+
+                    int result = int.Parse(subs[0]);
+                    int result2 = int.Parse(subs[1]);
+
+                    int result3 = result + result2;
+                    Console.WriteLine("Answer: " + result3);
+                }
+
+
+                // SUBTRACT
+                // --------------------------------------------------------------
+
+                if (line.Contains("-"))
+                {
+
+                    string[] subs = line.Split('-');
+
+                    int result = int.Parse(subs[0]);
+                    int result2 = int.Parse(subs[1]);
+
+                    int result3 = result - result2;
+                    Console.WriteLine("Answer: " + result3);
+                }
+
+
+                // MULTIPLY
+                // --------------------------------------------------------------
+
+                if (line.Contains("*"))
+                {
+
+                    string[] subs = line.Split('*');
+
+                    int result = int.Parse(subs[0]);
+                    int result2 = int.Parse(subs[1]);
+
+                    int result3 = result * result2;
+                    Console.WriteLine("Answer: " + result3);
+                }
+
+
+                // DIVIDE
+                // --------------------------------------------------------------
+
+                if (line.Contains("/"))
+                {
+
+                    string[] subs = line.Split('/');
+
+                    int result = int.Parse(subs[0]);
+                    int result2 = int.Parse(subs[1]);
+
+                    float result3 = (float)result / (float)result2;
+                    Console.WriteLine("Answer: " + result3);
+                }
+
+
+                Console.WriteLine("\nCalculate again? (y/n)\n");
+                again = Console.ReadLine();
+            }
+        }
+    }
+}
